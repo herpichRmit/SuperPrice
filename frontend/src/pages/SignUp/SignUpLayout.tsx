5b054c0f3ea5c9643eb5225f9bed54f17a4afce5
[Vincent Daniele] <[s3780417@student.rmit.edu.au]>
2023-09-13 22:21:32 +1000

    Added SignUpLayout and css

---
+++ frontend/src/pages/SignUp/SignUpLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SignUpLayout.css'
+import UserInput from './SignUp'
+
+const SignUpLayout = () => {
+
+    return(
+    <div className='userArea'>
+        <UserInput/>
+    </div>
+    )
+}
+
+export default SignUpLayout;
