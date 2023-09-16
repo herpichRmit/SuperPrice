28001ae6a872ca71a81d7954fb59aa7fd865288b
[Vincent Daniele] <[s3780417@student.rmit.edu.au]>
2023-09-16 15:17:12 +1000

    Converted main database to h2

--- backend/src/main/resources/db/migration/V2__insert_books.sql
+++ backend/src/main/resources/db/migration/V2__insert_books.sql
@@ -1,1 +1,1 @@
-INSERT INTO product (title, store, price, item_description) VALUES ('Ajax spray and wipe', 'COLES', 15.43, "Kills 99.9% of germs");
+INSERT INTO product (title, store, price, item_description) VALUES ('Ajax spray and wipe', 'COLES', 15.43, 'Kills 99.9% of germs');
