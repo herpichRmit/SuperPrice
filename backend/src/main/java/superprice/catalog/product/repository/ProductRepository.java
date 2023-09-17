3f21c0a2991fb26a8e1fbafa9be7627512819431
herpichRmit <105478249+herpichRmit@users.noreply.github.com>
2023-09-17 19:15:20 +1000

    sql-testing

--- backend/src/main/java/superprice/catalog/product/repository/ProductRepository.java
+++ backend/src/main/java/superprice/catalog/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package superprice.catalog.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import superprice.catalog.product.model.Product;
@@ -14,5 +15,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByTitle(String title);
     List<Product> findByCategory(String category);
     List<Product> findByTitleContainingIgnoreCase(String title);
+
+    //@Query(value = "SELECT * FROM USERS u WHERE u.status = 1", nativeQuery = true)
+    @Query(
+        value = "SELECT * FROM product WHERE title = ?",
+        nativeQuery = true
+        )
+    List<Product> testSQL(String keyword);
+
 }
 
