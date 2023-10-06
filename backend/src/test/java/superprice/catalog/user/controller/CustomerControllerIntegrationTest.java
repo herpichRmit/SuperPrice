9039ac9f1c153e13f41dba84a39c3ff28d61562d
Charlie Earnshaw <s3943382@student.rmit.edu.au>
2023-10-06 16:46:24 +1100

    CustomerControllerIntegrationTests done

--- backend/src/test/java/superprice/catalog/user/controller/CustomerControllerIntegrationTest.java
+++ backend/src/test/java/superprice/catalog/user/controller/CustomerControllerIntegrationTest.java
@@ -1,5 +1,72 @@
 package superprice.catalog.user.controller;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import superprice.catalog.Application;
+import superprice.catalog.user.model.Customer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@AutoConfigureMockMvc
 public class CustomerControllerIntegrationTest {
     
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    Flyway flyway;
+
+    @BeforeEach
+    private void setUp(){
+        flyway.migrate();
+    }
+
+    //get customer should start empty 
+    @Test
+    void getCustomer() throws Exception{
+        mvc.perform(get("/api/v1/users").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(
+            content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.length()").value(0)); //Should be no customers at the star
+    } 
+
+    //signup should create new customer 
+    @Test
+    void signUp() throws Exception{
+        mvc.perform(post("/api/v1/users/signup").content(asJsonString(new Customer(null, "Charles", "Earnshaw", "charlesearnshaw@myspace.com", "password1234!")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").exists())
+        .andExpect(jsonPath("$.firstName", is("Charles")));
+    }
+
+    //AsjsonString
+    public static String asJsonString(final Object obj) {
+        try {
+          return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+    }
 }
