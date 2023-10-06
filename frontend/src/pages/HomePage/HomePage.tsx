88b99b54e0ed849404c65ea9eda77f716b3b2d07
herpichRmit <105478249+herpichRmit@users.noreply.github.com>
2023-10-06 13:17:09 +1100

    Removed search bar bugs, removed need for addtional search button

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,4 @@
+"use client"
 import NavBar from '../../components/NavBar/NavBar';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import CategoriesMenu from '../../components/CategoriesMenu/CategoriesMenu';
