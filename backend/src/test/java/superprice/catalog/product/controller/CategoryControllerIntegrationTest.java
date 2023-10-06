147093f9a9eadf4379e0299faf106c48eb8ba861
Charlie Earnshaw <s3943382@student.rmit.edu.au>
2023-10-06 17:27:57 +1100

    Created productControllerIntegrationTest file

--- backend/src/test/java/superprice/catalog/product/controller/CategoryControllerIntegrationTest.java
+++ backend/src/test/java/superprice/catalog/product/controller/CategoryControllerIntegrationTest.java
@@ -1,5 +1,60 @@
 package superprice.catalog.product.controller;
 
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import superprice.catalog.Application;
+import superprice.catalog.user.model.Customer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = Application.class)
+@AutoConfigureMockMvc
 public class CategoryControllerIntegrationTest {
     
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    Flyway flyway;
+
+    @BeforeEach
+    private void setUp(){
+        flyway.migrate();
+    }
+
+    //get categories reutnrs categories
+    @Test
+    void all() throws Exception{
+        mvc.perform(get("/api/catalog/category")).andExpect(status().isOk())
+        .andExpect(
+            content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.length()").value(3)); //Should be no customers at the star
+    }
+
+    //AsjsonString
+    public static String asJsonString(final Object obj) {
+        try {
+          return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+    }
 }
