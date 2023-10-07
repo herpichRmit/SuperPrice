dad4fbe0b98201b6a94dc4697e3144c35eae9387
Charlie Earnshaw <s3943382@student.rmit.edu.au>
2023-10-07 14:44:29 +1100

    Added product controller unit tests

--- backend/src/test/java/superprice/catalog/product/controller/ProductControllerTest.java
+++ backend/src/test/java/superprice/catalog/product/controller/ProductControllerTest.java
@@ -1,11 +1,13 @@
 package superprice.catalog.product.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,4 +50,59 @@ public class ProductControllerTest {
             .thenReturn(new ArrayList<>());
         assertEquals(0, this.controller.getProduct().size());
     } 
+
+    //find id should return product 
+
+    //find id should return empty when product not available 
+
+    @Test
+    void getID_should_returnProduct_when_productAvaialble(){
+        when(this.service.getProduct(1L))
+        .thenReturn(Optional.of(new Product("Cheese", "Woolies", "A2 Milk", "Dairy", 5.00, "cheesy")));
+        Product product = this.controller.get(1L);
+        assertNotNull(product);
+        assertEquals("Cheese", product.getTitle());
+    } 
+
+    //compare by title should return title
+    @Test
+    void getProductsByTitle_should_returnProduct_when_productAvaialble(){
+        when(this.service.getProductsByTitle("Cheese"))
+        .thenReturn(List.of(new Product("Cheese", "Woolies", "A2 Milk", "Dairy", 5.00, "cheesy")));
+        List<Product> product = this.controller.get("Cheese");
+        assertNotNull(product);
+        assertEquals("Cheese", product.get(0).getTitle());
+    } 
+
+    //get product by keyword should return product
+    @Test
+    void getProductsByKeyword_should_returnProduct_when_productAvaialble(){
+        when(this.service.getProductsByKeyword("Cheese"))
+        .thenReturn(List.of(new Product("Cheese", "Woolies", "A2 Milk", "Dairy", 5.00, "cheesy")));
+        List<Product> product = this.controller.getByKeyword("Cheese");
+        assertNotNull(product);
+        assertEquals("Cheese", product.get(0).getTitle());
+    } 
+
+    //get category should return products for category
+    @Test
+    void getByCategory_should_returnProduct_when_productAvaialble(){
+        when(this.service.getProductsByCategory("Dairy"))
+        .thenReturn(List.of(new Product("Cheese", "Woolies", "A2 Milk", "Dairy", 5.00, "cheesy")));
+        List<Product> product = this.controller.getByCategory("Dairy");
+        assertNotNull(product);
+        assertEquals("Cheese", product.get(0).getTitle());
+    } 
+
+    //getbyprice should return products 
+    @Test
+    void getByPrice_should_returnProduct_when_productAvaialble(){
+        when(this.service.sortbyPrice("Cheese"))
+        .thenReturn(List.of(new Product("Cheese", "Woolies", "A2 Milk", "Dairy", 5.00, "cheesy")));
+        List<Product> product = this.controller.getByPrice("Cheese");
+        assertNotNull(product);
+        assertEquals("Cheese", product.get(0).getTitle());
+    } 
+
+
 }
